bdb404e1fb89f8c4ecbd70d177005f0a475b02e3
edrisyawar <159342807+edrisyawar@users.noreply.github.com>
2024-05-06 02:41:42 -0400

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,99 @@
+package music;
+
+public class Song{
+	private String id;
+	private String genre;
+	private String name;
+	private String[] artist;
+	private String album;
+	private Integer popularity;
+	private Long duration;
+	private double dance;
+	private double energy;
+	private double loudness;
+	private double accoustic;
+	private double instrumental;
+	private double liveness;
+	private double valence; 
+	private double speechiness;
+	private double tempo;
+
+	public Song(String id, String genre, String name, String[] artist, String album, Integer popularity, Long duration, double dance, double loudness, double accoustic, double instrumental, double liveness, double valence, double speechiness, double tempo) {
+		this.id = id;
+		this.genre = genre;
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.popularity = popularity;
+		this.duration = duration;
+		this.loudness =loudness;
+		this.accoustic = accoustic;
+		this.instrumental = instrumental;
+		this.liveness = liveness;
+		this.valence = valence;
+		this.speechiness = speechiness;
+		this.tempo = tempo;
+
+	public String get_name() {
+		return name;
+	}
+	public String get_id() {
+		return name;
+	}
+	public String get_genre() {
+		return name;
+	}
+	public String[] get_artist() {
+		return name;
+	}
+	public String get_album() {
+		return name;
+	}
+	public Integer get_popularity() {
+		return name;
+	}
+	public Long get_duration() {
+		return name;
+	}
+	public double get_dance() {
+		return name;
+	}
+	public double get_loudness() {
+		return name;
+	}
+	public double get_acoustic() {
+		return name;
+	}
+	public double get_instrumental() {
+		return name;
+	}
+	public double get_liveness() {
+		return name;
+	}
+	public double get_valence() {
+		return name;
+	}
+	public double get_speechiness() {
+		return name;
+	}
+	public double get_tempo() {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
